72c4ae48a47137a8dff60f0bc3bbce882fb71a63
Akash Biswas <akash41bt@gmail.com>
2025-09-21 16:10:54 +0600

    implement successfully

---
+++ Main.java
@@ -0,0 +1,48 @@
+interface Ebook{
+    void display();
+}
+
+class RealEBook implements Ebook{
+    String bookName;
+
+    RealEBook(String bookName){
+        this.bookName = bookName;
+        loadBook(bookName);
+    }
+
+    public void loadBook(String bookName){
+        System.out.println("Book loaded from disk: "+ bookName);
+    }
+
+    public void display() {
+        System.out.println("Displaying book: "+ bookName);
+    }
+}
+
+class ProxyEBook implements Ebook{
+    String bookName;
+    RealEBook realEBook;
+
+    ProxyEBook(String bookName){
+        this.bookName = bookName;
+    }
+
+    public void display() {
+        if(realEBook == null){
+            realEBook = new RealEBook(bookName);
+        }
+        realEBook.display();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Ebook ebook1 = new ProxyEBook("Physics");
+        Ebook ebook2 = new ProxyEBook("Math");
+
+        ebook1.display();
+        ebook1.display(); 
+
+        ebook2.display();
+    }
+}
